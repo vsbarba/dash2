9a6b6b483ee16e9ee8e83eab2c37002846ebf444
Victor Soto Barba <vsbarba@gmail.com>
2014-01-31 16:03:33 +0800

    added so many stuffs

---
+++ app/assets/js/blurrr.js
@@ -0,0 +1,20 @@
++ function($) {
+  "use strict";
+
+  // Blurr Public Class Definition
+  // =============================
+
+  var Blurrr = function() {
+    this.init();
+  }
+
+  Blurrr.prototype.init = function() {
+    $('[data-plugin="blurrr"]');
+  }
+  
+  // Blurrr Plugin Definition
+  // =============================
+
+
+
+}(window.jQuery); 
