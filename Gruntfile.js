9e436740e1409b52efef05f8e9dfd2c5a43bed62
Victor Soto Barba <vsbarba@gmail.com>
2014-01-31 16:29:23 +0800

    font_awesome

--- Gruntfile.js
+++ Gruntfile.js
@@ -260,6 +260,7 @@ module.exports = function (grunt) {
             // Explicitly add any files your site needs for distribution here.
             '_bower_components/jquery/jquery.js',
             '_bower_components/font-awesome/*',
+            '_bower_components/font-awesome/fonts/*',
             'favicon.ico',
             'apple-touch*.png'
           ],
